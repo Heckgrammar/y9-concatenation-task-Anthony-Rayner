96d88625056508df5079aa788d3880b1325b9bae
Anthony Rayner <23ARayner@heckgrammar.co.uk>
2026-02-02 09:47:11 +0000

    .

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
-﻿namespace Y9ConcatenationTaskAGS
+﻿namespace ConcatenationTaskAGS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string FirstName;
+            // INPUT
             Console.Write("Enter first name: ");
-            FirstName = Console.ReadLine();
-            // add two more variables for the users last name and their age
-            // take these inputs into the program
-            // use the test data Bob, Smith, 14
-            // ammend this code to output in one command
-            // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
-            // save and commit your program
-            // add evidence of the test input and your own input running the program 
+            string firstName = Console.ReadLine();
+            Console.Write("Enter second name: ");
+            string secondName = Console.ReadLine();
+            Console.Write("Enter age: ");
+            string age = Console.ReadLine();
+
+            // OUTPUT
+            Console.WriteLine("Hello " + firstName + " " + secondName + " you are " + age + " years old");
         }
     }
 }
